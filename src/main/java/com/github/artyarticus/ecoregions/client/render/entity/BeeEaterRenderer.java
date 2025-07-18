79a3a7b46e8adfab3992717dd44d4302c16b15d0
HelloCrossy <117866534+HelloCrossy@users.noreply.github.com>
2025-07-18 18:12:41 +0100

    bee eater baby scaling

--- src/main/java/com/github/artyarticus/ecoregions/client/render/entity/BeeEaterRenderer.java
+++ src/main/java/com/github/artyarticus/ecoregions/client/render/entity/BeeEaterRenderer.java
@@ -13,7 +13,7 @@ public class BeeEaterRenderer extends ZawaMobRenderer<BeeEaterEntity, BeeEaterMo
 
     @Override
     protected void scale(BeeEaterEntity entity, MatrixStack matrixStack, float partialTickTime) {
-        float scale = entity.isBaby() ? 0.4F : 0.75F;
+        float scale = entity.isBaby() ? 0.5F : 0.75F;
         matrixStack.scale(scale, scale, scale);
         super.scale(entity, matrixStack, partialTickTime);
     }
