064855877aa2dc4fa63fcdcc0f032c41c9286153
HelloCrossy <117866534+HelloCrossy@users.noreply.github.com>
2026-08-05 20:39:30 +0100

    remove unused classes & jsons

--- src/main/java/com/github/artyarticus/ecoregions/EcoRegions.java
+++ src/main/java/com/github/artyarticus/ecoregions/EcoRegions.java
@@ -3,7 +3,6 @@ package com.github.artyarticus.ecoregions;
 import com.github.artyarticus.ecoregions.block.EcoRegionsBlocks;
 import com.github.artyarticus.ecoregions.entity.EcoRegionsEntities;
 import com.github.artyarticus.ecoregions.item.EcoRegionsItems;
-import com.github.artyarticus.ecoregions.sounds.EcoRegionsSounds;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -21,7 +20,6 @@ public class EcoRegions {
         EcoRegionsEntities.REGISTRY.initialize();
         EcoRegionsItems.REGISTRAR.register(bus);
         EcoRegionsBlocks.REGISTRAR.register(bus);
-        EcoRegionsSounds.REGISTRAR.register(bus);
 
         bus.addListener(this::setup);
         bus.addListener(this::setupClient);
